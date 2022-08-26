ab71bce526b7b4f650e2d7b1cd12a2818d7253ea
Keaton Krieger <keatonkrieger@Keatons-MacBook-Pro.local>
2022-08-26 11:00:01 -0700

    Added services

--- src/components/service/service-default.js
+++ src/components/service/service-default.js
@@ -12,65 +12,65 @@ export default function Service({ ActiveIndex }) {
     }
     const service = [
         {
-            img: "img/news/1.jpg",
+            img: "img/news/dapps.webp",
             svg: <SVG_Custom1 />,
-            text: "Developing website involves creating your website, making it one of the most vital parts of your marketing",
+            text: "Dapps are decentrailized applications that run on peer to peer networks like Ethereum using smart contracts to interact with the data",
             date: "August 9, 2021",
-            title: "3D Animation",
-            text1: "Cavani is a leading web design agency with an award-winning design team that creates innovative, effective websites that capture your brand, improve your conversion rates, and maximize your revenue to help grow your business and achieve your goals.",
-            text2: "In today's digital world, your website is the first interaction consumers have with your business. That's why almost 95 percent of a user's first impression relates to web design. It's also why web design services can have an immense impact on your company's bottom line.",
-            text3: "That's why more companies are not only reevaluating their website's design but also partnering with Kura, the web design agency that's driven more than $2.4 billion in revenue for its clients. With over 50 web design awards under our belt, we're confident we can design a custom website that drives sales for your unique business."
+            title: "DAPPS",
+            text1: "A dapp has its backend code running on a decentralized peer-to-peer network. Contrast this with an app where the backend code is running on centralized servers.",
+            text2: "Some benefits of dapps are zero downtime, privacy, resistance to censorship, and complete data integrity. ",
+            text3: "Like anything theres drawbacks like maintance, performace overhead, network congestion, user experience and centralization."
         },
         {
-            img: "img/news/2.jpg",
+            img: "img/news/erc721.png",
             svg: <SVG_Custom2 />,
-            text: "Developing website involves creating your website, making it one of the most vital parts of your marketing",
+            text: "ERC721 aka NFT's aka Non-Fungible Tokens are unquie and have diffrent values of other tokens on the same contract",
             date: "August 9, 2021",
-            title: "Explainer Videos",
-            text1: "Cavani is a leading web design agency with an award-winning design team that creates innovative, effective websites that capture your brand, improve your conversion rates, and maximize your revenue to help grow your business and achieve your goals.",
+            title: "ERC721",
+            text1: "NFTS have changed the world in the small time they have been around. They created the ability for digital assests to be specifically owned by a certian person. No coping or forging since they are stored in the blockchain.",
             text2: "In today's digital world, your website is the first interaction consumers have with your business. That's why almost 95 percent of a user's first impression relates to web design. It's also why web design services can have an immense impact on your company's bottom line.",
             text3: "That's why more companies are not only reevaluating their website's design but also partnering with Kura, the web design agency that's driven more than $2.4 billion in revenue for its clients. With over 50 web design awards under our belt, we're confident we can design a custom website that drives sales for your unique business."
         },
         {
-            img: "img/news/3.jpg",
+            img: "img/news/smartContracts.webp",
             svg: <SVG_Custom3 />,
-            text: "Developing website involves creating your website, making it one of the most vital parts of your marketing",
+            text: "Smart contracts are programs that run on blockchains like Ethereum. They are a collection of code and data that is stored at a specific address on the blockchain",
             date: "August 9, 2021",
-            title: "App Development",
+            title: "Smart Contracts",
             text1: "Cavani is a leading web design agency with an award-winning design team that creates innovative, effective websites that capture your brand, improve your conversion rates, and maximize your revenue to help grow your business and achieve your goals.",
             text2: "In today's digital world, your website is the first interaction consumers have with your business. That's why almost 95 percent of a user's first impression relates to web design. It's also why web design services can have an immense impact on your company's bottom line.",
             text3: "That's why more companies are not only reevaluating their website's design but also partnering with Kura, the web design agency that's driven more than $2.4 billion in revenue for its clients. With over 50 web design awards under our belt, we're confident we can design a custom website that drives sales for your unique business."
         },
         {
-            img: "img/news/4.jpg",
+            img: "img/news/testing.webp",
             svg: <SVG_Custom4 />,
-            text: "Developing website involves creating your website, making it one of the most vital parts of your marketing",
+            text: "Smart contract testing is code that evaluates smart contracts for the quality of code during development. Smart Contracts are more permanent then regualar code",
             date: "August 9, 2021",
-            title: "Educational Video",
+            title: "Testing",
             text1: "Cavani is a leading web design agency with an award-winning design team that creates innovative, effective websites that capture your brand, improve your conversion rates, and maximize your revenue to help grow your business and achieve your goals.",
             text2: "In today's digital world, your website is the first interaction consumers have with your business. That's why almost 95 percent of a user's first impression relates to web design. It's also why web design services can have an immense impact on your company's bottom line.",
             text3: "That's why more companies are not only reevaluating their website's design but also partnering with Kura, the web design agency that's driven more than $2.4 billion in revenue for its clients. With over 50 web design awards under our belt, we're confident we can design a custom website that drives sales for your unique business."
         },
-        {
-            img: "img/news/5.jpg",
-            svg: <SVG_Custom5 />,
-            text: "Developing website involves creating your website, making it one of the most vital parts of your marketing",
-            date: "August 9, 2021",
-            title: "3D Game Art",
-            text1: "Cavani is a leading web design agency with an award-winning design team that creates innovative, effective websites that capture your brand, improve your conversion rates, and maximize your revenue to help grow your business and achieve your goals.",
-            text2: "In today's digital world, your website is the first interaction consumers have with your business. That's why almost 95 percent of a user's first impression relates to web design. It's also why web design services can have an immense impact on your company's bottom line.",
-            text3: "That's why more companies are not only reevaluating their website's design but also partnering with Kura, the web design agency that's driven more than $2.4 billion in revenue for its clients. With over 50 web design awards under our belt, we're confident we can design a custom website that drives sales for your unique business."
-        },
-        {
-            img: "img/news/6.jpg",
-            svg: <SVG_Custom6 />,
-            text: "Developing website involves creating your website, making it one of the most vital parts of your marketing",
-            date: "August 9, 2021",
-            title: "Presentations",
-            text1: "Cavani is a leading web design agency with an award-winning design team that creates innovative, effective websites that capture your brand, improve your conversion rates, and maximize your revenue to help grow your business and achieve your goals.",
-            text2: "In today's digital world, your website is the first interaction consumers have with your business. That's why almost 95 percent of a user's first impression relates to web design. It's also why web design services can have an immense impact on your company's bottom line.",
-            text3: "That's why more companies are not only reevaluating their website's design but also partnering with Kura, the web design agency that's driven more than $2.4 billion in revenue for its clients. With over 50 web design awards under our belt, we're confident we can design a custom website that drives sales for your unique business."
-        }
+        // {
+        //     img: "img/news/5.jpg",
+        //     svg: <SVG_Custom5 />,
+        //     text: "Developing website involves creating your website, making it one of the most vital parts of your marketing",
+        //     date: "August 9, 2021",
+        //     title: "3D Game Art",
+        //     text1: "Cavani is a leading web design agency with an award-winning design team that creates innovative, effective websites that capture your brand, improve your conversion rates, and maximize your revenue to help grow your business and achieve your goals.",
+        //     text2: "In today's digital world, your website is the first interaction consumers have with your business. That's why almost 95 percent of a user's first impression relates to web design. It's also why web design services can have an immense impact on your company's bottom line.",
+        //     text3: "That's why more companies are not only reevaluating their website's design but also partnering with Kura, the web design agency that's driven more than $2.4 billion in revenue for its clients. With over 50 web design awards under our belt, we're confident we can design a custom website that drives sales for your unique business."
+        // },
+        // {
+        //     img: "img/news/6.jpg",
+        //     svg: <SVG_Custom6 />,
+        //     text: "Developing website involves creating your website, making it one of the most vital parts of your marketing",
+        //     date: "August 9, 2021",
+        //     title: "Presentations",
+        //     text1: "Cavani is a leading web design agency with an award-winning design team that creates innovative, effective websites that capture your brand, improve your conversion rates, and maximize your revenue to help grow your business and achieve your goals.",
+        //     text2: "In today's digital world, your website is the first interaction consumers have with your business. That's why almost 95 percent of a user's first impression relates to web design. It's also why web design services can have an immense impact on your company's bottom line.",
+        //     text3: "That's why more companies are not only reevaluating their website's design but also partnering with Kura, the web design agency that's driven more than $2.4 billion in revenue for its clients. With over 50 web design awards under our belt, we're confident we can design a custom website that drives sales for your unique business."
+        // }
     ]
     return (
         <>
